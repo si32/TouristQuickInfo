2f98e65bbe1df9b42d70ba14e0c7df35033bf5fc
si32 <si32cs50@gmail.com>
2023-01-10 15:17:23 +0300

    refresh each weather separetly

--- tourist_quick_info/infopage/static/infopage/js/weather.js
+++ tourist_quick_info/infopage/static/infopage/js/weather.js
@@ -3,61 +3,72 @@ document.addEventListener('DOMContentLoaded', function() {
     let curent_weather_divs = document.querySelectorAll('.current-weather');
     curent_weather_divs.forEach(curent_weather_div => {
 
-        let city_name = curent_weather_div.dataset.city;
-        let country_name = curent_weather_div.dataset.country;
+        update_weather(curent_weather_div);
 
-        let csrftoken = getCookie('csrftoken');
-        fetch('/weather', {
-            method: "POST",
-            headers: {'X-CSRFToken': csrftoken},
-            mode: 'same-origin',
-            body: JSON.stringify({
-                city: city_name,
-                country: country_name
-            })
-        })
-        .then(result => result.json())
-        .then(data => {
-            let temp = document.createElement('div');
-            temp.setAttribute('class', 'temp');
-            temp.innerHTML = `
-                <div>
-                    <b title='Temperature'>T: </b>${data.current.temp_c}&#8451; / ${data.current.temp_f}&#8457;
-                </div>
-                <div>
-                    <b title='Wind'>W: </b>${data.current.wind_kph} k/h
-                    <b title='Humidity'>H: </b>${data.current.humidity} %
-                </div>`;
-            curent_weather_div.appendChild(temp);
-
-            let condition = document.createElement('div');
-            condition.setAttribute('class', 'condition');
-            condition.innerHTML = `
-                <img src=${data.current.condition.icon} title=${data.current.condition.text}>
-                ${data.current.condition.text}`;
-            curent_weather_div.appendChild(condition);
-
-            let forecast = document.createElement('div');
-            forecast.setAttribute('class', 'forecast');
-            curent_weather_div.appendChild(forecast);
-            let forecastdays = data.forecast.forecastday;
-            forecastdays.forEach(forecastday => {
-                let fday = document.createElement('div');
-                fday.setAttribute('class', 'forecastday');
-                let date = new Date(forecastday.date);
-                fday.innerHTML = `
-                    <div>${date.toLocaleDateString('en-EN', {weekday:'short'})} ${date.toLocaleDateString('sv-SE', {month:'numeric', day:'numeric'})}</div>
-                    <div>
-                    <img src=${forecastday.day.condition.icon} title=${forecastday.day.condition.text}>
-                    </div>
-                    <div>${forecastday.day.avgtemp_c}&#8451;</div>
-                `
-                forecast.appendChild(fday);
-            })
+    })
+})
 
 
+function refresh_weather(event) {
+    let btn = event.target;
+    let curent_weather_div = btn.previousElementSibling;
+    update_weather(curent_weather_div);
 
+}
+
+function update_weather(curent_weather_div) {
+    curent_weather_div.innerHTML = '';
+
+    let city_name = curent_weather_div.dataset.city;
+    let country_name = curent_weather_div.dataset.country;
+
+    let csrftoken = getCookie('csrftoken');
+    fetch('/weather', {
+        method: "POST",
+        headers: {'X-CSRFToken': csrftoken},
+        mode: 'same-origin',
+        body: JSON.stringify({
+            city: city_name,
+            country: country_name
         })
     })
+    .then(result => result.json())
+    .then(data => {
+        let temp = document.createElement('div');
+        temp.setAttribute('class', 'temp');
+        temp.innerHTML = `
+            <div>
+                <b title='Temperature'>T: </b>${data.current.temp_c}&#8451; / ${data.current.temp_f}&#8457;
+            </div>
+            <div>
+                <b title='Wind'>W: </b>${data.current.wind_kph} k/h
+                <b title='Humidity'>H: </b>${data.current.humidity} %
+            </div>`;
+        curent_weather_div.appendChild(temp);
 
-})
+        let condition = document.createElement('div');
+        condition.setAttribute('class', 'condition');
+        condition.innerHTML = `
+            <img src=${data.current.condition.icon} title=${data.current.condition.text}>
+            ${data.current.condition.text}`;
+        curent_weather_div.appendChild(condition);
+
+        let forecast = document.createElement('div');
+        forecast.setAttribute('class', 'forecast');
+        curent_weather_div.appendChild(forecast);
+        let forecastdays = data.forecast.forecastday;
+        forecastdays.forEach(forecastday => {
+            let fday = document.createElement('div');
+            fday.setAttribute('class', 'forecastday');
+            let date = new Date(forecastday.date);
+            fday.innerHTML = `
+                <div>${date.toLocaleDateString('en-EN', {weekday:'short'})} ${date.toLocaleDateString('sv-SE', {month:'numeric', day:'numeric'})}</div>
+                <div>
+                <img src=${forecastday.day.condition.icon} title=${forecastday.day.condition.text}>
+                </div>
+                <div>${forecastday.day.avgtemp_c}&#8451;</div>
+            `
+            forecast.appendChild(fday);
+        })
+    })
+}
